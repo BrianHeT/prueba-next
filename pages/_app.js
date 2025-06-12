4935b04e1a8811562ce2b7c29bdb9240866b5082
BrianHeT <129415133+BrianHeT@users.noreply.github.com>
2025-06-12 19:53:55 -0300

    liquid glass
    test liquidglass

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "@/styles/globals.css"; // si usás Tailwind o tu CSS general
+import "@/styles/globals.css"; 
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
